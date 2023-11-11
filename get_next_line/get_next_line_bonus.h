282a45fe66a711d8abc5bd1ba2df7ef8f61360b5
MarioZurera <139723812+MarioZurera@users.noreply.github.com>
2023-11-11 20:16:32 +0100

    get_next_line mandatory and bonus
    This get_next_line passes all test, including fracinette strict.
    The timeouts in the strict mode can be check at the francinette
    repository fac, it's a well-known problem for this test.
    Added an static array of 256 file descriptors, the numbers is choosen
    using the command ulimit -n, which prints the maximum numbers of fd
    allowed to be open an a certain time.

--- get_next_line/get_next_line_bonus.h
+++ get_next_line/get_next_line_bonus.h
@@ -1,21 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mzurera- <mzurera-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 13:16:32 by mzurera-          #+#    #+#             */
-/*   Updated: 2023/11/11 20:03:09 by mzurera-         ###   ########.fr       */
+/*   Updated: 2023/11/11 20:04:06 by mzurera-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 80
 # endif /* BUFFER_SIZE */
+
+# ifndef FD_LIMIT
+#  define FD_LIMIT 256
+# endif /* FD_LIMIT */
 
 # include <unistd.h>
 # include <stdlib.h>
@@ -28,4 +32,4 @@ char			*ft_substr(const char *s, unsigned int start, unsigned int len);
 unsigned int	ft_strlen(const char *str);
 char			*get_next_line(int fd);
 
-#endif /* GET_NEXT_LINE_H */
+#endif /* GET_NEXT_LINE_BONUS_H */
