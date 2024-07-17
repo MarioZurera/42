4a9c7dd0384c679b7397f23cff4912670086a630
Mario Zurera De Alva <mzurera-@c3r9s1.42malaga.com>
2024-07-17 15:04:28 +0200

    Deep_free with norminette.
    Remove one space for norminette proposes

--- pipex/libft/ft_deep_free.c
+++ pipex/libft/ft_deep_free.c
@@ -6,7 +6,7 @@
 /*   By: mzurera- <mzurera-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 19:19:56 by mzurera-          #+#    #+#             */
-/*   Updated: 2024/07/16 21:34:56 by mzurera-         ###   ########.fr       */
+/*   Updated: 2024/07/17 14:59:14 by mzurera-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	*ft_deep_free(void **ptr, unsigned int depth)
 	while (depth > 1 && mem[i] != NULL)
 	{
 		if (depth > 2)
-			ft_deep_free((void **) (mem + i), depth - 1);
+			ft_deep_free((void **)(mem + i), depth - 1);
 		free(mem[i]);
 		i++;
 	}
