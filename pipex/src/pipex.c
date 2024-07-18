5bea12298ee384aad6cbbf2cbc2e7e0c9300d218
Mario Zurera De Alva <mzurera-@c3r4s5.42malaga.com>
2024-07-18 15:27:55 +0200

    Preparation for pipex bonus.
    Improved makefile to support bonus rule.

--- pipex/src/pipex.c
+++ pipex/src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: mzurera- <mzurera-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 20:25:00 by mzurera-          #+#    #+#             */
-/*   Updated: 2024/07/18 13:44:04 by mzurera-         ###   ########.fr       */
+/*   Updated: 2024/07/18 15:16:07 by mzurera-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,11 @@ int	main(int argc, char **argv, char **envp)
 	unsigned int	num_commands;
 	int				status;
 
-	if (argc < 3)
+	if (argc != 5)
+	{
+		ft_printf("Usage: %s <infile> <command1> <command2> <outfile>\n", argv[0]);
 		return (0);
+	}
 	num_commands = (argc - 3);
 	pipex = ft_init_pipex(argv, envp, num_commands);
 	if (pipex == NULL)
